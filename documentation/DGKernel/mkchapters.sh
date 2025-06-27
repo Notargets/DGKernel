364e9c6a0daa8c732f459941820c450d37f99076
Luke Lonergan <mynameis2056@yahoo.com>
2025-06-27 10:37:07 -0700

    utility

---
+++ documentation/DGKernel/mkchapters.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+path="documentation/DGKernel"
+
+# Create parent directory if it doesn't exist
+mkdir -p "$path"
+
+for dir in \
+"0 - Introduction" \
+"1 - Element Definition" \
+"2 - Operators" \
+"3 - Meshes - Connecting the Elements to Input" \
+"3 - Fields - Input and Output" \
+"4 - Kernel Building" \
+"5 - Methods - Putting it all together" \
+"A - Example - Scalar Burgers Equation" \
+"B - Example - Eulers Equations" \
+"C - Example - Navier-Stokes Equations" \
+"D - Example - Maxwells Equations"
+do
+    echo "$path/$dir"
+    mkdir -p "$path/$dir"
+done
+
+# Optional: Verify the directories were created
+echo -e "\nCreated directories:"
+ls -la "$path/"
