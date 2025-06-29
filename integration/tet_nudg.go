3c3580536afde83d581ee1686c2b704b73cc7914
Luke Lonergan <mynameis2056@yahoo.com>
2025-06-29 16:37:28 -0700

    Implement interfaces

--- integration/tet_nudg.go
+++ integration/tet_nudg.go
@@ -1,12 +1,48 @@
 package integration
 
 import (
+	"github.com/notargets/DGKernel/element"
 	"github.com/notargets/gocfd/DG3D/mesh/readers"
 	"github.com/notargets/gocfd/DG3D/tetrahedra/tetelement"
 )
 
 type TetNudg struct {
 	*tetelement.Element3D
+}
+
+func (t TetNudg) GetReferenceElement() element.ReferenceElement {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (t TetNudg) GetGeometricTransform() element.GeometricTransform {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (t TetNudg) GetSurfaceGeometry() element.SurfaceGeometry {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (t TetNudg) GetProperties() element.ElementProperties {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (t TetNudg) GetReferenceGeometry() element.ReferenceGeometry {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (t TetNudg) GetNodalModal() element.NodalModalMatrices {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (t TetNudg) GetReferenceOperators() element.ReferenceOperators {
+	// TODO implement me
+	panic("implement me")
 }
 
 func NewTetNudg(order int, meshfile string) (tn *TetNudg) {
